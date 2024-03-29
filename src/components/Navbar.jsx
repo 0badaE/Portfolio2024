9b7765e7ab278607704cde4cba9173d0dcbc362b
Obi.É <Obada.Erfan97@gmail.com>
2024-03-29 14:15:28 -0400

    fixed small big

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ function Navbar(){
         },
     ]
     return(
-        <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+        <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed z-10">
             <div>
                 <h1 
                 className="text-5xl font-signature ml-2 cursor-pointer"
